b80576bb755abffae9b95626e617c39a9ce0da9a
James <nvrnight@gmail.com>
2015-07-26 22:50:14 -0500

    Created Solution/Projects. Created a function for reading delimited file rows.

---
+++ src/FParser.Tests/FReader.Tests.fs
@@ -0,0 +1,33 @@
+﻿module FReader
+
+open NUnit.Framework
+open FsUnit
+open System.Reflection
+open System.IO
+open FReader
+
+let GetLineFromTestFile fileName rowNumber =
+    let resourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames()
+    use stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fileName)
+    use reader = new StreamReader(stream)
+    let rec readUntilDone reader row =
+        let line = ReadLineDelimited reader '"'
+        if row = 1 then line else (readUntilDone reader (row-1))
+
+    readUntilDone reader rowNumber
+
+let GetLineFromMultiLineRowsTestFile rowNumber =
+    GetLineFromTestFile "MultiLineRows.csv" rowNumber
+
+[<Test>]
+let ``ReadLineDelimited will read lines with multiline text values`` () =
+    GetLineFromMultiLineRowsTestFile 2 |> should equal "t,\"t\n\nt\",t"
+
+[<Test>]
+let ``ReadLineDelimited will read lines with escaped text qualifiers`` () =
+    GetLineFromMultiLineRowsTestFile 4 |> should equal "t,\"t\"\"t\"\"t\",\"\""
+
+[<Test>]
+let ``ReadLineDelimited will read lines with no text qualifiers`` () =
+    GetLineFromMultiLineRowsTestFile 1 |> should equal "col1,col2,col3"
+    GetLineFromMultiLineRowsTestFile 3 |> should equal "t,t,t"
---
+++ src/FParser/FReader.fs
@@ -0,0 +1,24 @@
+﻿module FReader
+
+open System.IO
+
+let ReadLineDelimited (reader:StreamReader) (textQualifier:char) =
+    let rec lineHasUnclosedTextQualifier (textQualifier:char) (line:string) (textQualifierIsOpened:bool) =
+        match line.Length with
+        | 0 ->
+            textQualifierIsOpened
+        | _ ->
+            let first = line.[0]
+            let stillOpened =  (not textQualifierIsOpened && first = textQualifier) || (textQualifierIsOpened && first <> textQualifier)
+
+            lineHasUnclosedTextQualifier textQualifier line.[1..line.Length - 1] stillOpened
+
+    let rec readUntilFinished (reader:StreamReader) (textQualifier:char) (currentLine:string) =
+        let line = currentLine + reader.ReadLine()
+        match (reader.Peek() > -1 && lineHasUnclosedTextQualifier textQualifier line false) with
+        | false ->
+            line
+        | true ->
+            readUntilFinished reader textQualifier (if reader.Peek() > -1 then line + "\n" else "")
+
+    readUntilFinished reader textQualifier ""
