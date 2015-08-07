f50859d56a14dea57d9eca3f0d6e1e8715f94995
James <nvrnight@gmail.com>
2015-08-06 23:59:13 -0500

    Expanded Delimited String Parser functionality to be more comprehensive.

--- src/FParser.Tests/DelimitedStringParser.Tests.fs
+++ src/FParser.Tests/DelimitedStringParser.Tests.fs
@@ -2,42 +2,141 @@
 
 open FsUnit
 open NUnit.Framework
+
 open DelimitedStringParser
 
-let validateString = ValidateDelimitedString ',' '"'
+let validateString = TestableValidateDelimitedString ',' '"'
+let success = { Result = Success; FailureReason = None; FieldNumber = 0 }
+
+let failure = { Result = Failed; FailureReason = None; FieldNumber = 0 }
+let failureTooMany = { failure with FailureReason = TooManyColumns }
+let failureNotEnough = { failure with FailureReason = NotEnoughColumns }
+let failureMissingTextQualifier fieldNumber = 
+    { failure with FailureReason = MissingRequiredTextQualifier; FieldNumber = fieldNumber }
+
+let validateCharacterSuccessFunc = fun (a:char) (b:char) (c:TextOption) (d:bool) (e:char option) (f:char) (g:char option) -> None
+
+[<Test>]
+let ``ValidateCharacterFromDelimitedString should succeed with valid parameters`` () =
+    ValidateCharacterFromDelimitedString ',' '"' Required true (Some 'a') 'b' (Some 'c') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required false (Some 'a') 'b' (Some 'c') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required true (Some '"') '"' (Some '"') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required true (Some '"') ',' (Some 'g') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required false (Some '"') '"' (Some '"') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required true (Some 'a') '"' (Some '"') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required false (Some '"') '"' (Some 'a') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required false (Some 'g') '"' (Some '"') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required true (Some ',') '"' (Some '"') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required false (Some '"') '"' (Some ',') |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required true (Some ',') '"' Option.None |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Required false (Some '"') ',' Option.None |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Banned false Option.None ',' Option.None |> should equal None
+    ValidateCharacterFromDelimitedString ',' '"' Banned false (Some ',') '"' (Some '"') |> should equal None
+
+[<Test>]
+let ``ValidateCharacterFromDelimitedString should fail with TextQualifierNotEscaped when the current character is an unescaped text qualifier`` () =
+    ValidateCharacterFromDelimitedString ',' '"' Required true (Some ',') '"' (Some 'g') |> should equal TextQualifierNotEscaped
+    ValidateCharacterFromDelimitedString ',' '"' Required false (Some 'g') '"' (Some 'g') |> should equal TextQualifierNotEscaped
+
+[<Test>]
+let ``ValidateCharacterFromDelimitedString should fail with MissingClosingTextQualifier when text is open and it's the last character and is not the textQualifier`` () =
+    ValidateCharacterFromDelimitedString ',' '"' Required true (Some ',') ',' Option.None
+    |> should equal (MissingClosingTextQualifier)
+
+[<Test>]
+let ``ValidateCharacterFromDelimitedString should fail with ContainsBannedTextQualifier when text is not open and previous character is delimiter and text qualifier is banned and present and next character does not escape it`` () =
+    ValidateCharacterFromDelimitedString ',' '"' Banned false (Some ',') '"' (Some 'g')
+    |> should equal (ContainsBannedTextQualifier)
+
+[<Test>]
+let ``ValidateCharacterFromDelimitedString should fail with MissingRequiredTextQualifier when text is not open and previous character is delimiter and text qualifier is required and missing`` () =
+    ValidateCharacterFromDelimitedString ',' '"' Required false (Some ',') 'f' (Some 'g')
+    |> should equal (MissingRequiredTextQualifier)
+
+[<Test>]
+let ``ValidateCharacterFromDelimitedString should fail with MissingRequiredTextQualifier when there is no previous character and text qualifier is required and missing`` () =
+    ValidateCharacterFromDelimitedString ',' '"' Required false Option.None ',' Option.None
+    |> should equal (MissingRequiredTextQualifier)
+
+[<Test>]
+let ``ValidateCharacterFromDelimitedString should fail with ContainsBannedTextQualifier when there is no previous character and text qualifier is banned and exists`` () =
+    ValidateCharacterFromDelimitedString ',' '"' Banned false Option.None '"' Option.None
+    |> should equal (ContainsBannedTextQualifier)
+
+[<Test>]
+let ``ValidateDelimitedString should fail when second field has a failing character validation`` () =
+    validateString
+        "\"\",foo,"
+        [ { TextQualifier = Required }; { TextQualifier = Required }; { TextQualifier = Required } ]
+        (fun (a) (b) (textQualifierOption) (textOpened) (previous) (current) (next) ->
+            match (textQualifierOption, textOpened, previous = Some ',', current = 'f', next = Some 'o') with
+            | (Required, false, true, true, true) -> MissingRequiredTextQualifier
+            | (_, _, _, _, _) -> None
+        )
+    |> should equal (failureMissingTextQualifier 2)
+
+[<Test>]
+let ``ValidateDelimitedString should fail when first field has a failing character validation`` () =
+    validateString
+         ",foo,"
+         [ { TextQualifier = Required }; { TextQualifier = Required }; { TextQualifier = Required } ]
+         (fun (a) (b) (textQualifierOption) (textOpened) (previous) (current) (next) ->
+            match (textQualifierOption, textOpened, previous = Option.None, current = ',', next = Some 'f') with
+            | (Required, false, true, true, true) -> MissingRequiredTextQualifier
+            | (_, _, _, _, _) -> None
+         )
+    |> should equal (failureMissingTextQualifier 1)
 
 [<Test>]
 let ``ValidateDelimitedString with basic fields`` () =
-    validateString ",foo," 1 |> should equal false
-    validateString ",foo," 3 |> should equal true
-    validateString ",foo," 2 |> should equal false
+    let r1 = validateString ",foo," [ { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    r1 |> should equal failureTooMany
+    let r2 = validateString ",foo," [ { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    r2 |> should equal failureTooMany
+    let r3 = validateString ",foo," [ { TextQualifier = Optional }; { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    r3|> should equal success
 
 [<Test>]
 let ``ValidateDelimitedString with basic quoted field`` () =
-    validateString ",\"foo\"," 1 |> should equal false
-    validateString ",\"foo\"," 2 |> should equal false
-    validateString ",\"foo\"," 3 |> should equal true
+    validateString ",\"foo\"," [ { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureTooMany
+    validateString ",\"foo\"," [ { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureTooMany
+    validateString ",\"foo\"," [ { TextQualifier = Optional }; { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal success
 
 [<Test>]
 let ``ValidateDelimitedString with embedded newline`` () =
-    validateString ",\"foo\nbar\"" 1 |> should equal false
-    validateString ",\"foo\nbar\"" 2 |> should equal true
-    validateString ",\"foo\nbar\"" 3 |> should equal false
+    validateString ",\"foo\nbar\"" [ { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureTooMany
+    validateString ",\"foo\nbar\"" [ { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal success
+    validateString ",\"foo\nbar\"" [ { TextQualifier = Optional }; { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureNotEnough
 
 [<Test>]
 let ``ValidateDelimitedString with embedded delimiter`` () =
-    validateString ",\"foo,bar\"" 1 |> should equal false
-    validateString ",\"foo,bar\"" 2 |> should equal true
-    validateString ",\"foo,bar\"" 3 |> should equal false
+    validateString ",\"foo,bar\"" [ { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureTooMany
+    validateString ",\"foo,bar\"" [ { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal success
+    validateString ",\"foo,bar\"" [ { TextQualifier = Optional }; { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureNotEnough
 
 [<Test>]
 let ``ValidateDelimitedString with embedded text qualifier`` () =
-    validateString ",\"foo\"\"bar\"" 1 |> should equal false
-    validateString ",\"foo\"\"bar\"" 2 |> should equal true
-    validateString ",\"foo\"\"bar\"" 3 |> should equal false
+    validateString ",\"foo\"\"bar\"" [ { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureTooMany
+    validateString ",\"foo\"\"bar\"" [ { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal success
+    validateString ",\"foo\"\"bar\"" [ { TextQualifier = Optional }; { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureNotEnough
 
 [<Test>]
 let ``ValidateDelimitedString with with empty string`` () =
-    validateString ",\"\"," 1 |> should equal false
-    validateString ",\"\"," 2 |> should equal false
-    validateString ",\"\"," 3 |> should equal true
+    validateString ",\"\"," [ { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureTooMany
+    validateString ",\"\"," [ { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal failureTooMany
+    validateString ",\"\"," [ { TextQualifier = Optional }; { TextQualifier = Optional }; { TextQualifier = Optional } ] validateCharacterSuccessFunc
+    |> should equal success
--- src/FParser/DelimitedStringParser.fs
+++ src/FParser/DelimitedStringParser.fs
@@ -1,17 +1,73 @@
 ﻿module DelimitedStringParser
 
-let ValidateDelimitedString (delimiter:char) (textQualifier:char) (value:string) (columnCount:int) =
-    let rec countFields value delimiter textQualifier increment textOpened =
-        if increment > columnCount then increment
-        else match value with
-        | "" ->
-            increment+1
-        | _ ->
-            let first = value.[0]
-            countFields (value.[1..value.Length-1])
-                delimiter
-                textQualifier
-                (if first = delimiter && not textOpened then increment+1 else increment)
-                (if first = textQualifier then not textOpened else textOpened)
-    
-    countFields value delimiter textQualifier 0 false = columnCount
+open System
+
+type TextOption = Required | Banned | Optional
+type FileField = { TextQualifier:TextOption }
+
+type ValidationResult = Success | Failed
+type ValidationFailureReason = MissingRequiredTextQualifier | MissingClosingTextQualifier | TextQualifierNotEscaped | ContainsBannedTextQualifier | NotEnoughColumns | TooManyColumns | TextQualifierUnescapedOrMissingDelimiter | None
+type FileFieldValidationResult = { Result:ValidationResult; FailureReason:ValidationFailureReason; FieldNumber:int }
+
+let ValidateCharacterFromDelimitedString (delimiter:char) (textQualifier:char) (textQualifierOption:TextOption) (textOpened:bool) (previous:char option) (current:char) (next:char option) =
+    let previousIsDelimiter = previous = Some delimiter
+    let previousIsTextQualifier = previous = Some textQualifier
+    let previousIsNone = previous = Option.None
+    let currentIsTextQualifier = current = textQualifier
+    let nextIsTextQualifier = next = Some textQualifier
+    let nextIsDelimiter = next = Some delimiter
+    let nextIsNone = next = Option.None
+    match (textOpened, nextIsNone, currentIsTextQualifier, previousIsDelimiter) with
+    | (true, true, false, _) -> MissingClosingTextQualifier
+    | (_, false, false, false) -> None
+    | (_, _, _, _) ->
+        match (textQualifierOption, textOpened, previousIsDelimiter, previousIsTextQualifier, previousIsNone, currentIsTextQualifier, nextIsTextQualifier, nextIsDelimiter, nextIsNone) with
+        | (Required, _, false, false, true, false, _, _, _) -> MissingRequiredTextQualifier
+        | (Required, false, true, _, _, false, _, _, _) -> MissingRequiredTextQualifier
+        | (Banned, _, false, false, true, true, _, _, _) -> ContainsBannedTextQualifier
+        | (Banned, false, true, _, _, true, false, _, _) -> ContainsBannedTextQualifier
+        | (_, true, _, _, _, false, false, false, true) -> MissingClosingTextQualifier
+        | (_, false, false, false, _, true, false, _, _) -> TextQualifierNotEscaped
+        | (_, true, _, _, _, true, false, false, false) -> TextQualifierNotEscaped
+        | (_, _, _, _, _, _, _, _, _) -> None
+
+let TestableValidateDelimitedString (delimiter:char) (textQualifier:char) (value:string) (fields:list<FileField>) validateCharacterFunc =
+    let rec validateFields value delimiter textQualifier (fields:list<FileField>) currentField textOpened (previous:char option) =
+        let failResult = { Result = Failed; FailureReason = None; FieldNumber = 0; }
+        
+        let nextCharacter (value:string) =
+            if value.Length > 1 then Some(value.[1]) else Option.None
+        
+        match currentField > fields.Length with
+        | true -> { failResult with FailureReason = TooManyColumns; }
+        | false ->
+            match value with
+            | "" ->
+                match currentField < fields.Length with
+                | true -> { failResult with FailureReason = NotEnoughColumns; }
+                | false -> { Result = Success; FailureReason = None; FieldNumber = 0 }
+            | _ ->
+                let current = value.[0]
+                let next = nextCharacter value
+                let textQualifierOption = fields.[currentField-1].TextQualifier
+
+                let characterValidationResult = validateCharacterFunc delimiter textQualifier textQualifierOption textOpened previous current next
+                    
+                match characterValidationResult = None with
+                | false -> { failResult with FailureReason = characterValidationResult; FieldNumber = currentField }
+                | true ->
+                    let isNewField = current = delimiter && not textOpened
+
+                    validateFields (value.[1..value.Length-1])
+                        delimiter
+                        textQualifier
+                        fields
+                        (if isNewField then (currentField + 1) else currentField)
+                        (if current = textQualifier then not textOpened else textOpened)
+                        (Some current)
+                    
+
+    validateFields value delimiter textQualifier fields 1 false Option.None
+
+let ValidateDelimitedString (delimiter:char) (textQualifier:char) (value:string) (fields:list<FileField>) =
+    TestableValidateDelimitedString delimiter textQualifier value fields ValidateCharacterFromDelimitedString
